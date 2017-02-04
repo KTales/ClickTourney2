97614f885244877ac7c3b159eb6c300f679b3788
Connor Skidmore <connorskiidmore@gmail.com>
2017-02-04 06:50:44 -0500

    Front end 'improvements'

--- ClickTourney/ClickTourney/App_Start/BundleConfig.cs
+++ ClickTourney/ClickTourney/App_Start/BundleConfig.cs
@@ -25,7 +25,8 @@ namespace ClickTourney
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+					  "~/Content/clicktourney.css"));
         }
     }
 }
