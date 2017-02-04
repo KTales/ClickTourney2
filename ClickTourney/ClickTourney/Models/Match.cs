7ea5d4b5d85ba282189111d8b2502f3cf4e2b22a
ktales <keegantales188@gmail.com>
2017-02-04 04:05:10 -0500

    Add tourney and match models

---
+++ ClickTourney/ClickTourney/Models/Match.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ClickTourney.Models
+{
+    public class Match
+    {
+        public int MatchId { get; set; }
+
+        public string Player1 { get; set; }
+        public string Player2 { get; set; }
+
+        public virtual Tournament Tournament { get; set; }
+    }
+}
---
+++ ClickTourney/ClickTourney/Models/Tournament.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+
+namespace ClickTourney.Models
+{
+    public class Tournament
+    {
+        public Tournament()
+        {
+            this.createMatches();
+        }
+
+        private void createMatches()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int TournamentId { get; set; }
+
+        public virtual ApplicationUser Owner { get; set; }
+
+        public string Name { get; set; }
+
+        public virtual ICollection<Match> Matches { get; set; }
+
+        [DisplayName("# of Participants")]
+        public int PlayerCount { get; set; }
+
+        [DisplayName("Type")]
+        public string TournamentType { get; set; }
+    }
+}
