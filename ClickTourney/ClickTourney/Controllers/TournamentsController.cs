4c1aea3e7979479a8df0686c426f611039b991cd
ktales <keegantales188@gmail.com>
2017-02-10 19:37:20 -0500

    Restrict access to Tournaments

--- ClickTourney/ClickTourney/Controllers/TournamentsController.cs
+++ ClickTourney/ClickTourney/Controllers/TournamentsController.cs
@@ -16,12 +16,14 @@ namespace ClickTourney.Controllers
         private ApplicationDbContext db = new ApplicationDbContext();
 
         // GET: Tournaments
+        [Authorize]
         public ActionResult Index()
         {
             return View(db.Tournaments.ToList());
         }
 
         // GET: Tournaments/Details/5
+        [Authorize]
         public ActionResult Details(int? id)
         {
             if (id == null)
@@ -37,6 +39,7 @@ namespace ClickTourney.Controllers
         }
 
         // GET: Tournaments/Create
+        [Authorize]
         public ActionResult Create()
         {
             IEnumerable<string> ValidTypes = new List<string> { "Round Robin", "Double Round Robin", "Elimination" };
@@ -49,6 +52,7 @@ namespace ClickTourney.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
+        [Authorize]
         public ActionResult Create([Bind(Include = "TournamentId,Name,PlayerCount,TournamentType")] Tournament tournament)
         {
             string ownerId = User.Identity.GetUserId();
@@ -66,6 +70,7 @@ namespace ClickTourney.Controllers
         }
 
         // GET: Tournaments/Edit/5
+        [Authorize]
         public ActionResult Edit(int? id)
         {
             if (id == null)
@@ -85,6 +90,7 @@ namespace ClickTourney.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
+        [Authorize]
         public ActionResult Edit([Bind(Include = "TournamentId,Name,PlayerCount,TournamentType")] Tournament tournament)
         {
             if (ModelState.IsValid)
@@ -97,6 +103,7 @@ namespace ClickTourney.Controllers
         }
 
         // GET: Tournaments/Delete/5
+        [Authorize]
         public ActionResult Delete(int? id)
         {
             if (id == null)
@@ -114,6 +121,7 @@ namespace ClickTourney.Controllers
         // POST: Tournaments/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
+        [Authorize]
         public ActionResult DeleteConfirmed(int id)
         {
             Tournament tournament = db.Tournaments.Find(id);
